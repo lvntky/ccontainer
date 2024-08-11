96c39bddc5d1b0c62868cf46c3a48c3de7dff176
lvntky <klevent1903@gmail.com>
2024-08-11 10:47:02 +0300

    [ci] i hate ci

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -5,7 +5,7 @@ if(BUILD_TESTING)
   add_subdirectory(test)
 endif()
 
-option(BUILD_MCSS_DOCS "Build documentation using Doxygen and m.css" OFF)
+option(BUILD_MCSS_DOCS "Build documentation using Doxygen and m.css" ON)
 if(BUILD_MCSS_DOCS)
   include(cmake/docs.cmake)
 endif()
