c9a3269e29d06bd0a572f0d0c8bb072155c6bd20
lvntky <klevent1903@gmail.com>
2024-08-11 19:29:29 +0300

    [build] added conan

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,1 +1,4 @@
+include(CMakeFindDependencyMacro)
+find_dependency(json-c)
+
 include("${CMAKE_CURRENT_LIST_DIR}/ccontainerTargets.cmake")
--- test/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build/
-cmake ..
-cd ..
-cmake --build build --config Release
-./build/ccontainer_tests
