6818b8e695fd209a2ecf2381593a8d59bd4a01a5
lvntky <klevent1903@gmail.com>
2024-08-12 11:49:27 +0300

    [feature] script to create one single header file

---
+++ amalgamate.py
@@ -0,0 +1,9 @@
+# -- ccontainer amalgamete helper script --
+# This helper script iterates include/ccontainer directory
+# Copies all header files
+# And creates one single .h file which named `ccontainer_amalgamate.h` out of it
+# Into the curret directory or specified directory
+# By this way you can use all the container types without 
+# explicity including them.
+
+print("hello world")
