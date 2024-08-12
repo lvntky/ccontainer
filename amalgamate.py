8e1d9caa62c8e5bdfd3484a0d95ce17b99f13743
lvntky <klevent1903@gmail.com>
2024-08-12 19:08:24 +0300

    [feature] amalgamate script added

--- amalgamate.py
+++ amalgamate.py
@@ -3,7 +3,20 @@
 # Copies all header files
 # And creates one single .h file which named `ccontainer_amalgamate.h` out of it
 # Into the curret directory or specified directory
-# By this way you can use all the container types without 
+# By this way you can use all the container types without
 # explicity including them.
 
-print("hello world")
+import os
+
+ccontainer_amalgamate_header = open("ccontainer_amalgamate.h", 'a') # use append
+INCLUDE_PATH =  "./include/ccontainer/"
+
+for header in os.listdir(INCLUDE_PATH):
+    if header.endswith(".h"):
+        # copy the content
+        # write to amalgamate header
+        header_content = open(INCLUDE_PATH + header, 'r')
+        ccontainer_amalgamate_header.write(header_content.read())
+        continue
+    else :
+        continue
